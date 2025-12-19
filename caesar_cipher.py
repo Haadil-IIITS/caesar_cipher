555b5741f99737c319c384d643a002045674d206
Haadil basha <haadilbasha@gmail.com>
2025-12-19 23:49:51 +0530

    code is written a bit optimised

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,21 +1,12 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 word=str()
-def encrypt(word):
+def caeser(word,j):
     for i in text:
-        index = alphabet.index(i)+shift  
-        
+        index = alphabet.index(i)+int((shift*j))
         if index > 25:
-            #index=index-26                 #u can try different logic by using modulo (index % len(alphabet))        
-            index=index%len(alphabet)       # so basically it will bring the letter to the start
+            index=index-26                
             word = word + alphabet[index]
-        else:
-            word = word + alphabet[index]
-    print(word)
-def decrypt(word):
-    for i in text:
-        index = alphabet.index(i)-shift  
-        index=index % len(alphabet)
-        if index <0:
+        elif index <0:
              index=index+26
              word = word + alphabet[index]
         else:
@@ -27,7 +18,7 @@ while a=='y':
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
     if direction=="encode":
-        encrypt(word)
+        caeser(word,1)
     else:
-        decrypt(word)
+        caeser(word,-1)
     a=str(input("do u want to continue:(y/n)"))
